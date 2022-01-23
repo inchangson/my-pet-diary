b1826b508c1bd95da0c10e66e29786a3cb2aa158
inchangson <3721cha21.11@gmail.com>
2022-01-23 22:16:16 +0900

    modify db bulletinfeed

--- bulletin/models.py
+++ bulletin/models.py
@@ -13,6 +13,9 @@ class BulletinFeed(models.Model):
 #    id = models.IntegerField(primary_key=True)
     title       = models.CharField(max_length=255,  null=True)
     upload_time = models.DateTimeField( null=True)
-    txt_path    = models.CharField(max_length=255,  null=True)
+    
+    #txt_path로 바꿀지 확인
+    content    = models.TextField(max_length=255,  null=True)
+    
     img_path    = models.CharField(max_length=255,  null=True)
     user        = models.ForeignKey(TempUser, on_delete=models.SET_NULL, null = True)
